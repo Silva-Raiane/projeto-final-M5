7f42f4d303599b3324e6501095628cd7a2240d20
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-04-25 20:55:57 +0100

    feat(User.service): creating service for user

---
+++ server/src/services/User.service.js
@@ -0,0 +1,105 @@
+import { UserModel } from "../models/User.model.js"
+import { database } from "../database/connection.js"
+import { ERROS, SUCCESS } from "../shared/messages.js"
+
+export class UserService{
+    async createUser(username, email, password){
+        try{
+            await database.sync()
+            const newUser = await UserModel.create({username, email, password})
+            return {
+                statusValue: 201,
+                message: `Created ${SUCCESS.USER}`,
+                userId: newUser.id
+            }
+        }catch(error){
+            return {
+                statusValue: 404,
+                message: error.message
+            }
+        }
+    }
+
+    async getUser(id){
+        try{
+            await database.sync()
+            const user = await UserModel.findByPk(id)
+            return {
+                statusValue:200,
+                message: `Returned ${SUCCESS.USER}`,
+                user: user
+            }
+        }catch(error){
+            return {
+                statusValue:404,
+                message: error.message
+            }
+        }
+    }
+
+    async updatePassword(id, password, newPassword){
+        try{
+            await database.sync()
+            const user = await UserModel.findByPk(id)
+            if(user.password === password){
+                user.update({password:newPassword})
+                return {
+                    statusValue: 200,
+                    message: `Password ${SUCCESS.UPDATED}`
+                }
+            }
+    
+            return {
+                statusValue: 404,
+                message: ERROS.WRONG_PASSWORD
+            }
+        }catch(error){
+            return {
+                statusValue: 404,
+                message: error.message
+            }
+        }
+    }
+
+    async updateUsername(id, password, newUsername){
+        try{
+            await database.sync()
+            const user = await UserModel.findByPk(id)
+            if(user.password === password){
+                user.update({username: newUsername})
+                return {
+                    statusValue: 200,
+                    message: `Username ${SUCCESS.UPDATED}`
+                }
+            }
+    
+            return {
+                statusValue: 404,
+                message: ERROS.WRONG_PASSWORD
+            }
+        }catch(error){
+            return {
+                statusValue: 404,
+                message: error.message
+            }
+        }
+    }
+
+    async deleteUser(id){
+        try{
+            await database.sync()
+            const user = await UserModel.findByPk(id)
+            user.destroy()
+            return {
+                statusValue: 200,
+                message: `User ${SUCCESS.DELETED}`
+            }
+        }catch(error){
+            return {
+                statusValue: 404,
+                message: error.message
+            }
+        }
+
+    }
+}

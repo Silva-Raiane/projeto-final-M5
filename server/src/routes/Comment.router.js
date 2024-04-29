e8de6c4a71a344613ccb2b661295761ccf55cc6e
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-04-29 20:20:59 +0100

    feat(Comment.router): creating get comments end.
    Creating an endpoint for getting a post's comments.

--- server/src/routes/Comment.router.js
+++ server/src/routes/Comment.router.js
@@ -1,4 +1,4 @@
-import { createComment, deleteComment } from "../controllers/Comment.controller.js"
+import { createComment, getCommentsFromPost, deleteComment } from "../controllers/Comment.controller.js"
 import { CommentValidator } from "../middleware/Comment.validator.js"
 import { Router } from "express"
 
@@ -7,6 +7,9 @@ const instanceCommentValidator = new CommentValidator()
 
 routerComment.post("/create-comment", instanceCommentValidator.createCommentValidation, async (req, res)=>{
     return await createComment(req, res)
+})
+routerComment.get("/comments/get-from-post", async (req, res) => {
+    return await getCommentsFromPost(req, res)
 })
 routerComment.delete("/comments/delete", async (req, res)=>{
     return await deleteComment(req, res)

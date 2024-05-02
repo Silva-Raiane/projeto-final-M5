577fc2bb998ddc8da2164c0e2d0c6e1d78e2f42f
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-05-02 19:44:31 +0100

    feat(Authenticator): creating func verify JWT

--- server/src/authentication/Authenticator.js
+++ server/src/authentication/Authenticator.js
@@ -7,3 +7,21 @@ export function generateAccessToken(userId){
     return jwt.sign({ userId: userId }, process.env.TOKEN, {expiresIn: '1h'})
 }
 
+export function verifyJWT(req, res, next){
+    try{
+        const token = req.headers['x-access-token']
+
+        jwt.verify(token, secret, (error, decoded) => {
+            if(error) return res.status(401).json({
+                error: error
+            });
+
+            req.userId = decoded.userId;
+            next();
+        }) 
+    }catch(error){
+        return res.status(401).json({
+            message: "Error while trying to verify JWT."
+        })
+    }
+}

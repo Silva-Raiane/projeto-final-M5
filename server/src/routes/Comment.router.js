8823810f5da76fb5b1fec8e1db074cb73e4450d5
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-05-03 18:23:16 +0100

    feat(Comments.router): adding JWT verifier

--- server/src/routes/Comment.router.js
+++ server/src/routes/Comment.router.js
@@ -1,3 +1,4 @@
+import { verifyJWT } from "../authentication/Authenticator.js"
 import { createComment, getCommentsFromPost, deleteComment } from "../controllers/Comment.controller.js"
 import { CommentValidator } from "../middleware/Comment.validator.js"
 import { Router } from "express"
@@ -5,13 +6,13 @@ import { Router } from "express"
 const routerComment = Router()
 const instanceCommentValidator = new CommentValidator()
 
-routerComment.post("/create-comment", instanceCommentValidator.createCommentValidation, async (req, res)=>{
+routerComment.post("/create-comment", verifyJWT,instanceCommentValidator.createCommentValidation, async (req, res)=>{
     return await createComment(req, res)
 })
-routerComment.get("/comments/get-from-post", instanceCommentValidator.getCommentsFromPostValidation, async (req, res) => {
+routerComment.get("/comments/get-from-post", verifyJWT, instanceCommentValidator.getCommentsFromPostValidation, async (req, res) => {
     return await getCommentsFromPost(req, res)
 })
-routerComment.delete("/comments/delete", instanceCommentValidator.deleteCommentValidation, async (req, res)=>{
+routerComment.delete("/comments/delete", verifyJWT, instanceCommentValidator.deleteCommentValidation, async (req, res)=>{
     return await deleteComment(req, res)
 })
 

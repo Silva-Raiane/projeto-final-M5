539097db3ffbfe090b47fe886ecb7ce50fab10e6
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-05-07 21:02:20 +0100

    feat(Cors): adding cors configuration

--- server/src/server.js
+++ server/src/server.js
@@ -3,11 +3,13 @@ import { routerUser } from "./routes/User.router.js"
 import { routerPost } from "./routes/Post.router.js"
 import { routerComment } from "./routes/Comment.router.js"
 import { tryToConnect } from "./database/connection.js"
+import cors from 'cors'
 
 const server = express()
 const port = 8001
 
 server.use(express.json())
+server.use(cors())
 server.use(routerUser)
 server.use(routerPost)
 server.use(routerComment)

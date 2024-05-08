711d68712a716f6fb80f77b1620c7dd6af3b7f82
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-05-08 22:28:19 +0100

    feat(Connection): setting the url to connect to db

--- server/src/database/connection.js
+++ server/src/database/connection.js
@@ -1,10 +1,6 @@
 import {Sequelize} from "sequelize";
 
-const database = new Sequelize("db_insight_bytes", "db_insight_bytes_user", "kquGYxi4oohKQbsB5qir3o6a0rTUsVCL", {
-    host: "postgres://db_insight_bytes_user:kquGYxi4oohKQbsB5qir3o6a0rTUsVCL@dpg-cotvb6icn0vc73e9fps0-a.oregon-postgres.render.com/db_insight_bytes",
-    dialect: "postgres",
-    logging: false
-});
+const database = new Sequelize("postgresql://db_insight_bytes_owner:uUIc8MZdBEN6@ep-late-boat-a5f0niug.us-east-2.aws.neon.tech/db_insight_bytes?sslmode=require");
 
 export async function tryToConnect(){
     try{

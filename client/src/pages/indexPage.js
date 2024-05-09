1f210a04ea5af17023270fac72d5dfb3e040da5e
Adson Gabriel <adsongabrielfsilva@gmail.com>
2024-05-09 15:39:30 +0100

    feat(Posts): getting posts from API

--- client/src/pages/indexPage.js
+++ client/src/pages/indexPage.js
@@ -1,13 +1,27 @@
+import { useState } from "react";
 import "../App.css";
 import Post from "../components/post.js";
+import axios from "axios";
 
-export default function IndexPage(){
-    return(
-        <div>
-            <h1>Posts</h1>
-            <Post />
-            <Post />
-            <Post />
-        </div>
-    );
+export default function IndexPage() {
+    const [posts, setPosts] = useState([]);
+
+    async function getPosts() {
+        axios.get("https://projeto-final-m5-15w5.onrender.com/posts/getAll").then(response => {
+            setPosts(response.data.response.posts)
+        })
+    }
+    
+    getPosts();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <>
+        {posts.map((item) => (
+          <Post key={item.id} title={item.title} createdAt={item.createdAt} content={item.content} />
+        ))}
+      </>
+    </div>
+  );
 }
